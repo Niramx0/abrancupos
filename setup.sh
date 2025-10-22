da0cf94b6325e9134b37da288b49c36afa5a4db1
Nicolás Pájaro Sánchez <npajaro@unal.edu.co>
2025-10-22 15:53:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 echo "Iniciando setup del proyecto..."
-# - 0. 
+# - 0. Cargar variables del entorno(.env)
+ENV_FILE="Proyecto/.env"
+if [ -f "$ENV_FILE" ]; then
+  echo "Cargando variables desde Proyecto/.env..."
+  set -a
+  source "$ENV_FILE"
+  set +a
+else
+  echo "No se encontró el archivo .env. Asegúrate de tenerlo antes de continuar"
+  exit 1
+fi
 
 # - 1. Levantar Base de datos con docker compose 
 if command -v docker-compose >/dev/null 2>&1; then
