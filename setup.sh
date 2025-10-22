05032dcd367d3124afdb7d719e45049aaf4840e2
Nicolás Pájaro Sánchez <npajaro@unal.edu.co>
2025-10-22 14:08:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+echo "Iniciando setup del proyecto..."
+# - 0. 
+
+# - 1. Levantar Base de datos con docker compose 
+if command -v docker-compose >/dev/null 2>&1; then
+  docker-compose up -d
+else
+  docker compose up -d
+fi
+
+echo "Setup finalizado correctamente..."
