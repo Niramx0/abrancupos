92acf9726bb921344685c12ada2f2e1968b99370
Nicolás Pájaro Sánchez <npajaro@unal.edu.co>
2025-10-23 22:51:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+set -e
 echo "Iniciando setup del proyecto..."
 # - 0. Cargar variables del entorno(.env)
 ENV_FILE="Proyecto/.env"
 if [ -f "$ENV_FILE" ]; then
-  echo "Cargando variables desde Proyecto/.env..."
+  echo "Cargando variables desde Proyecto/.env"
   set -a
   source "$ENV_FILE"
   set +a
@@ -14,10 +14,27 @@ else
 fi
 
 # - 1. Levantar Base de datos con docker compose 
+cd Proyecto 
 if command -v docker-compose >/dev/null 2>&1; then
   docker-compose up -d
 else
   docker compose up -d
 fi
+MYSQL_CONTAINER="mysql-dev"
 
+# - 2. Verificar si MySQL esta listo
+echo "Esperando a que MySQL acepte conexiones"
+until docker exec "$MYSQL_CONTAINER" mysqladmin ping -h "localhost" -p"$MYSQL_ROOT_PASSWORD" --silent; do
+  sleep 2
+done
+echo "MySQL está listo para aceptar conexiones."
+
+# - 3. Estado final de los contenedores
+if command -v docker-compose >/dev/null 2>&1; then
+  docker-compose ps
+else
+  docker compose ps
+fi
+ 
+cd ..
 echo "Setup finalizado correctamente..."
