c767f9ed66f399269bbf6b239e13c7613d022348
Nicolás Pájaro Sánchez <npajaro@unal.edu.co>
2025-11-25 21:06:57 -0500

    Update admin.py

--- Proyecto/SincroHealth/proyecto/polls/admin.py
+++ Proyecto/SincroHealth/proyecto/polls/admin.py
@@ -1,4 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Paciente, Medico, Administrador, CitaMedica, RecetaMedica, HistorialMedico
 
-# Register your models here.
-
+admin.site.register(Paciente)
+admin.site.register(Medico)
+admin.site.register(Administrador)
+admin.site.register(CitaMedica)
+admin.site.register(RecetaMedica)
+admin.site.register(HistorialMedico)
