c4953f4fae22503f795fe201e4c7478b8479d267
Niram <110203162+Niramx0@users.noreply.github.com>
2025-12-01 15:46:26 -0500

    Delete Proyecto/SincroHealth/proyecto directory

--- Proyecto/SincroHealth/proyecto/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("paciente/<int:idpaciente>/citas/", views.citas_paciente, name="citas_paciente"),
-    path("medico/<int:idmedico>/horario/", views.horario_medico, name="horario_medico"),
-    path("paciente/<int:idpaciente>/historial/", views.vista_historial, name="historial_html"),
-    path("paciente/<int:idpaciente>/historial/pdf/", views.historial_pdf, name="historial_pdf"),
-]
